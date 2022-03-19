616f1d5e7f4ec622232e22ef6c8bac9a67b0203b
hachangbeom <ha3938@naver.com>
2022-03-20 00:52:22 +0900

    [LAB01] lab01 submitted

---
+++ Source/Library/Game/Game.cpp
@@ -0,0 +1,284 @@
+#include "Game/Game.h"
+
+namespace library
+{
+    using namespace Microsoft::WRL;
+    /*--------------------------------------------------------------------
+      Global Variables
+    --------------------------------------------------------------------*/
+    HINSTANCE               g_hInst = nullptr;
+    HWND                    g_hWnd = nullptr;
+    D3D_DRIVER_TYPE         g_driverType = D3D_DRIVER_TYPE_NULL;
+    D3D_FEATURE_LEVEL       g_featureLevel = D3D_FEATURE_LEVEL_11_0;
+    ComPtr<ID3D11Device> g_pd3dDevice;
+    ComPtr<ID3D11Device1> g_pd3dDevice1;
+    ComPtr<ID3D11DeviceContext> g_pImmediateContext;
+    ComPtr<ID3D11DeviceContext1> g_pImmediateContext1;
+    ComPtr<IDXGISwapChain> g_pSwapChain;
+    ComPtr<IDXGISwapChain1> g_pSwapChain1;
+    ComPtr<ID3D11RenderTargetView> g_pRenderTargetView;
+    /*--------------------------------------------------------------------
+      TODO: Initialize global variables (remove the comment)
+    --------------------------------------------------------------------*/
+
+    /*--------------------------------------------------------------------
+      Forward declarations
+    --------------------------------------------------------------------*/
+    HRESULT InitWindow(HINSTANCE hInstance, int nCmdShow);
+    HRESULT InitDevice();
+    void CleanupDevice();
+    LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+    void Render();
+
+    /*F+F+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+      Function: WindowProc
+
+      Summary:  Defines the behavior of the window—its appearance, how
+                it interacts with the user, and so forth
+
+      Args:     HWND hWnd
+                  Handle to the window
+                UINT uMsg
+                  Message code
+                WPARAM wParam
+                  Additional data that pertains to the message
+                LPARAM lParam
+                  Additional data that pertains to the message
+
+      Returns:  LRESULT
+                  Integer value that your program returns to Windows
+    -----------------------------------------------------------------F-F*/
+    LRESULT CALLBACK WindowProc(_In_ HWND hWnd, _In_ UINT uMsg, _In_ WPARAM wParam, _In_ LPARAM lParam);
+
+    void Render()
+    {
+        // Just clear the backbuffer
+        g_pImmediateContext->ClearRenderTargetView(g_pRenderTargetView.Get(), Colors::MidnightBlue);
+        g_pSwapChain->Present(0, 0);
+    }
+
+    void CleanupDevice()
+    {
+        if (g_pImmediateContext) g_pImmediateContext->ClearState();
+
+    }
+    /*--------------------------------------------------------------------
+      TODO: Function definitions (remove this comment)
+    --------------------------------------------------------------------*/
+    HRESULT InitWindow(HINSTANCE hInstance, int nCmdShow)
+    {
+        // Register class
+        WNDCLASSEX wcex;
+        wcex.cbSize = sizeof(WNDCLASSEX);
+        wcex.style = CS_HREDRAW | CS_VREDRAW;
+        wcex.lpfnWndProc = WndProc;
+        wcex.cbClsExtra = 0;
+        wcex.cbWndExtra = 0;
+        wcex.hInstance = hInstance;
+        wcex.hIcon = LoadIcon(hInstance, (LPCTSTR)IDI_TUTORIAL1);
+        wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
+        wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+        wcex.lpszMenuName = nullptr;
+        wcex.lpszClassName = L"TutorialWindowClass";
+        wcex.hIconSm = LoadIcon(wcex.hInstance, (LPCTSTR)IDI_TUTORIAL1);
+        if (!RegisterClassEx(&wcex))
+            return E_FAIL;
+
+        // Create window
+        g_hInst = hInstance;
+        RECT rc = { 0, 0, 800, 600 };
+        AdjustWindowRect(&rc, WS_OVERLAPPEDWINDOW, FALSE);
+        g_hWnd = CreateWindow(L"TutorialWindowClass", L"Direct3D 11 Tutorial 1: Direct3D 11 Basics",
+            WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX,
+            CW_USEDEFAULT, CW_USEDEFAULT, rc.right - rc.left, rc.bottom - rc.top, nullptr, nullptr, hInstance,
+            nullptr);
+        if (!g_hWnd)
+            return E_FAIL;
+
+        ShowWindow(g_hWnd, nCmdShow);
+
+        return S_OK;
+    }
+
+
+    //--------------------------------------------------------------------------------------
+    // Called every time the application receives a message
+    //--------------------------------------------------------------------------------------
+    LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+    {
+        PAINTSTRUCT ps;
+        HDC hdc;
+
+        switch (message)
+        {
+        case WM_PAINT:
+            hdc = BeginPaint(hWnd, &ps);
+            EndPaint(hWnd, &ps);
+            break;
+
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            break;
+
+            // Note that this tutorial does not handle resizing (WM_SIZE) requests,
+            // so we created the window without the resize border.
+
+        default:
+            return DefWindowProc(hWnd, message, wParam, lParam);
+        }
+
+        return 0;
+    }
+
+
+    //--------------------------------------------------------------------------------------
+    // Create Direct3D device and swap chain
+    //--------------------------------------------------------------------------------------
+    HRESULT InitDevice()
+    {
+        HRESULT hr = S_OK;
+
+        RECT rc;
+        GetClientRect(g_hWnd, &rc);
+        UINT width = rc.right - rc.left;
+        UINT height = rc.bottom - rc.top;
+
+        UINT createDeviceFlags = 0;
+#ifdef _DEBUG
+        createDeviceFlags |= D3D11_CREATE_DEVICE_DEBUG;
+#endif
+
+        D3D_DRIVER_TYPE driverTypes[] =
+        {
+            D3D_DRIVER_TYPE_HARDWARE,
+            D3D_DRIVER_TYPE_WARP,
+            D3D_DRIVER_TYPE_REFERENCE,
+        };
+        UINT numDriverTypes = ARRAYSIZE(driverTypes);
+
+        D3D_FEATURE_LEVEL featureLevels[] =
+        {
+            D3D_FEATURE_LEVEL_11_1,
+            D3D_FEATURE_LEVEL_11_0,
+            D3D_FEATURE_LEVEL_10_1,
+            D3D_FEATURE_LEVEL_10_0,
+        };
+        UINT numFeatureLevels = ARRAYSIZE(featureLevels);
+
+        for (UINT driverTypeIndex = 0; driverTypeIndex < numDriverTypes; driverTypeIndex++)
+        {
+            g_driverType = driverTypes[driverTypeIndex];
+            hr = D3D11CreateDevice(nullptr, g_driverType, nullptr, createDeviceFlags, featureLevels, numFeatureLevels,
+                D3D11_SDK_VERSION, &g_pd3dDevice, &g_featureLevel, &g_pImmediateContext);
+
+            if (hr == E_INVALIDARG)
+            {
+                // DirectX 11.0 platforms will not recognize D3D_FEATURE_LEVEL_11_1 so we need to retry without it
+                hr = D3D11CreateDevice(nullptr, g_driverType, nullptr, createDeviceFlags, &featureLevels[1], numFeatureLevels - 1,
+                    D3D11_SDK_VERSION, &g_pd3dDevice, &g_featureLevel, &g_pImmediateContext);
+            }
+
+            if (SUCCEEDED(hr))
+                break;
+        }
+        if (FAILED(hr))
+            return hr;
+
+        // Obtain DXGI factory from device (since we used nullptr for pAdapter above)
+        ComPtr<IDXGIFactory1> dxgiFactory;
+        {
+            ComPtr<IDXGIDevice> dxgiDevice;
+            hr = g_pd3dDevice->QueryInterface(__uuidof(IDXGIDevice),(void**)&dxgiDevice);
+            if (SUCCEEDED(hr))
+            {
+                ComPtr<IDXGIAdapter> adapter;
+                hr = dxgiDevice->GetAdapter(&adapter);
+                if (SUCCEEDED(hr))
+                {
+                    hr = adapter->GetParent(__uuidof(IDXGIFactory1), (void**)&dxgiFactory);
+                }
+            }
+        }
+        if (FAILED(hr))
+            return hr;
+
+        // Create swap chain
+        ComPtr<IDXGIFactory2> dxgiFactory2;
+        hr = dxgiFactory->QueryInterface(__uuidof(IDXGIFactory2), (void**)&dxgiFactory2);
+        if (dxgiFactory2)
+        {
+            // DirectX 11.1 or later
+            hr = g_pd3dDevice.As(&g_pd3dDevice1);
+            if (SUCCEEDED(hr))
+            {
+                (void)g_pImmediateContext.As(&g_pImmediateContext1);
+            }
+
+            DXGI_SWAP_CHAIN_DESC1 sd = {};
+            sd.Width = width;
+            sd.Height = height;
+            sd.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+            sd.SampleDesc.Count = 1;
+            sd.SampleDesc.Quality = 0;
+            sd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+            sd.BufferCount = 1;
+
+            hr = dxgiFactory2->CreateSwapChainForHwnd (g_pd3dDevice.Get(), g_hWnd, &sd, nullptr, nullptr, g_pSwapChain1.GetAddressOf());
+            if (SUCCEEDED(hr))
+            {
+                hr = g_pSwapChain1.As(&g_pSwapChain);
+            }
+
+        }
+        else
+        {
+            // DirectX 11.0 systems
+            DXGI_SWAP_CHAIN_DESC sd = {};
+            sd.BufferCount = 1;
+            sd.BufferDesc.Width = width;
+            sd.BufferDesc.Height = height;
+            sd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+            sd.BufferDesc.RefreshRate.Numerator = 60;
+            sd.BufferDesc.RefreshRate.Denominator = 1;
+            sd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+            sd.OutputWindow = g_hWnd;
+            sd.SampleDesc.Count = 1;
+            sd.SampleDesc.Quality = 0;
+            sd.Windowed = TRUE;
+
+            hr = dxgiFactory->CreateSwapChain(g_pd3dDevice.Get(), &sd, g_pSwapChain.GetAddressOf());
+        }
+
+        // Note this tutorial doesn't handle full-screen swapchains so we block the ALT+ENTER shortcut
+        dxgiFactory->MakeWindowAssociation(g_hWnd, DXGI_MWA_NO_ALT_ENTER);
+
+
+        if (FAILED(hr))
+            return hr;
+
+        // Create a render target view
+        ComPtr<ID3D11Texture2D> pBackBuffer;
+        hr = g_pSwapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), (void**)&pBackBuffer);
+        if (FAILED(hr))
+            return hr;
+
+        hr = g_pd3dDevice->CreateRenderTargetView(pBackBuffer.Get(), nullptr, &g_pRenderTargetView);
+        if (FAILED(hr))
+            return hr;
+
+        g_pImmediateContext->OMSetRenderTargets(1, g_pRenderTargetView.GetAddressOf(), nullptr);
+
+        // Setup the viewport
+        D3D11_VIEWPORT vp;
+        vp.Width = (FLOAT)width;
+        vp.Height = (FLOAT)height;
+        vp.MinDepth = 0.0f;
+        vp.MaxDepth = 1.0f;
+        vp.TopLeftX = 0;
+        vp.TopLeftY = 0;
+        g_pImmediateContext->RSSetViewports(1, &vp);
+
+        return S_OK;
+    }
+
+}
